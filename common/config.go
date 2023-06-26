88ded3b80ddd614075c6b83a59dacaaa1efb41b5
Rhine Cliff <rhinecliff@protonmail.com>
2023-06-25 19:22:50 -0700

    feat[configuration]: Layout configuration parameters for control node

--- common/config.go
+++ common/config.go
@@ -1,5 +1,7 @@
 package common
 
+import "github.com/spf13/viper"
+
 // ===============================================================================
 // Common Submodule Config
 
@@ -63,7 +65,74 @@ type APIServerConfig struct {
 }
 
 // ===============================================================================
+// Persistence Configuration Structures
+
+// PostgresSSLConfig Postgres connection SSL config
+type PostgresSSLConfig struct {
+	// Enabled whether to enable SSL when connecting to Postgres
+	Enabled bool `mapstructure:"enabled" json:"enabled"`
+	// CAFile the CA cert file to challenge remote with
+	CAFile *string `mapstructure:"caFile" json:"caFile,omitempty" validate:"omitempty,file"`
+}
+
+// PostgresConfig Postgres connection config
+type PostgresConfig struct {
+	// Host Postgres server host
+	Host string `mapstructure:"host" json:"host" validate:"required"`
+	// Port Postgres server port
+	Port uint16 `mapstructure:"port" json:"port" validate:"lte=65535,gte=0"`
+	// Database the specific database to use
+	Database string `mapstructure:"db" json:"db" validate:"required"`
+	// User the user to connect with
+	User string `mapstructure:"user" json:"user" validate:"required"`
+	// SSL the connection SSL settings
+	SSL PostgresSSLConfig `mapstructure:"ssl" json:"ssl" validate:"required,dive"`
+}
+
+// ===============================================================================
+// GCP PubSub Configuration Structures
+
+// PubSubSubcriptionConfig PubSub subscription config
+type PubSubSubcriptionConfig struct {
+	// Topic the pubsub topic to subscribe to
+	Topic string `mapstructure:"topic" json:"topic" validate:"required"`
+	// MsgTTLInSec message retention within the subscription in secs
+	MsgTTLInSec uint32 `mapstructure:"msgTTL" json:"msgTTL" validate:"gte=600,lte=604800"`
+}
+
+// ReqRespClientConfig PubSub request-response client config
+type ReqRespClientConfig struct {
+	// GCPProject the GCP project operate in
+	GCPProject string `mapstructure:"gcpProject" json:"gcpProject" validate:"required"`
+	// InboudRequestTopic the PubSub topic this client listen on for inboud request
+	InboudRequestTopic PubSubSubcriptionConfig `mapstructure:"self" json:"self" validate:"required,dive"`
+	// SupportWorkerCount number of support workers to spawn to process inbound requests
+	SupportWorkerCount int `mapstructure:"supportWorkerCount" json:"supportWorkerCount" validate:"required"`
+	// RequestTimeoutEnforceIntInSec outbound request timeout enforcement check interval in secs
+	RequestTimeoutEnforceIntInSec uint32 `mapstructure:"requestTimeoutEnforceIntInSec" json:"requestTimeoutEnforceIntInSec" validate:"gte=15,lte=120"`
+}
+
+// ===============================================================================
+// Major System Component Configuration Structures
+
+// SystemManagementConfig define control node management sub-module config
+type SystemManagementConfig struct {
+	// APIServer management REST API server config
+	APIServer APIServerConfig `mapstructure:"api" json:"api" validate:"required,dive"`
+	// RRClient PibSub request-response client config
+	RRClient ReqRespClientConfig `mapstructure:"requestResponse" json:"requestResponse" validate:"required,dive"`
+}
+
+// ===============================================================================
 // Complete Configuration Structures
+
+// ControlNodeConfig define control node settings and behavior
+type ControlNodeConfig struct {
+	// Postgres postgres DB configuration
+	Postgres PostgresConfig `mapstructure:"postgres" json:"postgres" validate:"required,dive"`
+	// Management management
+	Management SystemManagementConfig `mapstructure:"management" json:"management" validate:"required,dive"`
+}
 
 // EdgeNodeConfig define edge node settings and behavior
 type EdgeNodeConfig struct {
@@ -73,3 +142,29 @@ type EdgeNodeConfig struct {
 
 // ===============================================================================
 // Default Configuration Setter
+
+// InstallDefaultControlNodeConfigValues installs default config parameters in viper for
+// control node
+func InstallDefaultControlNodeConfigValues() {
+	// Default Postgres config
+	viper.SetDefault("postgres.port", 5432)
+	viper.SetDefault("postgres.ssl.enabled", false)
+
+	// Default management sub-component
+	// Default management HTTP server config
+	viper.SetDefault("management.api.enabled", true)
+	viper.SetDefault("management.api.service.listenOn", "0.0.0.0")
+	viper.SetDefault("management.api.service.appPort", 8080)
+	viper.SetDefault("management.api.service.timeoutSecs.read", 60)
+	viper.SetDefault("management.api.service.timeoutSecs.write", 60)
+	viper.SetDefault("management.api.service.timeoutSecs.idle", 60)
+	viper.SetDefault("management.api.apis.endPoint.pathPrefix", "/")
+	viper.SetDefault("management.api.apis.requestLogging.requestIDHeader", "X-Request-ID")
+	viper.SetDefault("management.api.apis.requestLogging.skipHeaders", []string{
+		"WWW-Authenticate", "Authorization", "Proxy-Authenticate", "Proxy-Authorization",
+	})
+	// Default system manager request-response client config
+	viper.SetDefault("management.requestResponse.self.msgTTL", 600)
+	viper.SetDefault("management.requestResponse.supportWorkerCount", 4)
+	viper.SetDefault("management.requestResponse.requestTimeoutEnforceIntInSec", 30)
+}
--- go.mod
+++ go.mod
@@ -10,6 +10,7 @@ require (
 	github.com/google/uuid v1.3.0
 	github.com/gorilla/mux v1.8.0
 	github.com/oklog/ulid/v2 v2.1.0
+	github.com/spf13/viper v1.16.0
 	github.com/stretchr/testify v1.8.4
 	gorm.io/driver/sqlite v1.5.1
 	gorm.io/gorm v1.25.1
@@ -31,13 +32,22 @@ require (
 	github.com/google/s2a-go v0.1.4 // indirect
 	github.com/googleapis/enterprise-certificate-proxy v0.2.3 // indirect
 	github.com/googleapis/gax-go/v2 v2.9.1 // indirect
+	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/leodido/go-urn v1.2.4 // indirect
+	github.com/magiconair/properties v1.8.7 // indirect
 	github.com/mattn/go-sqlite3 v1.14.16 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/pelletier/go-toml/v2 v2.0.8 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/spf13/afero v1.9.5 // indirect
+	github.com/spf13/cast v1.5.1 // indirect
+	github.com/spf13/jwalterweatherman v1.1.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/stretchr/objx v0.5.0 // indirect
+	github.com/subosito/gotenv v1.4.2 // indirect
 	github.com/urfave/negroni v1.0.0 // indirect
 	go.opencensus.io v0.24.0 // indirect
 	golang.org/x/crypto v0.9.0 // indirect
@@ -51,5 +61,6 @@ require (
 	google.golang.org/genproto v0.0.0-20230410155749-daa745c078e1 // indirect
 	google.golang.org/grpc v1.55.0 // indirect
 	google.golang.org/protobuf v1.30.0 // indirect
+	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
